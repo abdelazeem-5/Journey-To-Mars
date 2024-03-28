72191576913ecac580d60b9f6251d8c3e5d8e5c9
abdelazeem-5 <154232833+abdelazeem-5@users.noreply.github.com>
2024-03-29 00:37:07 +0200

    Add files via upload

---
+++ x1.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+  float n, m, a, x, y;
+  cout << "enter n , m , a\n";
+  cin >> n >> m >> a;
+  x = n / a;
+  y = m / a;
+  cout << x * y ;
+  
+  return 0;
+}
+
